4dff583b3a8d20f3f3573372e62e29cbf9618fa2
Jiayu Fan <Klein2339@gmail.com>
2020-09-29 16:54:47 +0100

    Dynamic properties

--- fundamentals.js
+++ fundamentals.js
@@ -8,12 +8,28 @@ const aCar = {
     registration: { year: 201, county: "WD", number: 1058 }
 };
 
-console.log(aCar.owner + ' drives a ' + aCar.type.make);
+console.log(aCar.owner + " drives a " + aCar.type.make);
 console.log(
-    "Reg. = " +
-      aCar.registration.year +
-      "-" +
-      aCar.registration.county +
-      "-" +
-      aCar.registration.number
-  );
+  "Reg. = " +
+    aCar.registration.year +
+    "-" +
+    aCar.registration.county +
+    "-" +
+    aCar.registration.number
+);
+
+aCar.milage = 10000
+aCar.color = {
+  exterior: "red",
+  interior: { texture: "leather", shade: "cream" }
+}
+
+console.log(
+  "It is a " +
+    aCar.color.exterior +
+    " car, " +
+    aCar.milage +
+    " milage, with " +
+    aCar.color.interior.texture +
+    " interior."
+);
